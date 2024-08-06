a2347e070d6e8fbb318680a280a54c6f538ce3cc
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2024-08-06 09:44:38 +0530

    Created userModel

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userModel = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    phone_no:{
+        type:Number,
+        unique:true,
+        length:10
+    },
+    role:{
+        type:Number,
+        default:0
+    }
+},
+{timestamps:true});
+
+export default mongoose.Model('userModel',userModel);   
