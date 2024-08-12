ed7b8100527b718462298bb323614ad8e104b763
atharvarajurkar09 <atharva09rajurkar@gmail.com>
2024-08-12 22:52:36 +0530

    updated signup.js

--- todolist/src/Signup.js
+++ todolist/src/Signup.js
@@ -4,9 +4,10 @@ import './Signup.css';
 const Signup=()=>{
     const[name,setName]=useState("")
     const[email,setEmail]=useState("")
-    const[mobile_no,setMobile_no]=useState("")
+    const[phone_no,setPhone_no]=useState("")
     const[password,setPassword]=useState("")
-    const[cpassword,setCpassword]=useState("")
+    const[cfmpwd,setCfmpwd]=useState("")
+
     return(
         <body>
             <div class="signupContainer">{/* signup-container */}
@@ -15,9 +16,9 @@ const Signup=()=>{
                     <form>
                     <input type='text' className='signup-input' placeholder='First Name' value={name} onChange={(e)=>setName(e.target.value)} required />
                     <input type='email' className='signup-input' placeholder='Your Email Address' value={email} onChange={(e)=>setEmail(e.target.value)} required />
-                    <input type='number' className='signup-input' placeholder='Enter your mobile number' value={mobile_no} onChange={(e)=>setMobile_no(e.target.value)} required />
+                    <input type='number' className='signup-input' placeholder='Enter your mobile number' value={phone_no} onChange={(e)=>setPhone_no(e.target.value)} required />
                     <input type='password' className='signup-input' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} required />
-                    <input type='password' className='signup-input' placeholder='Confirm Password' value={cpassword} onChange={(e)=>setCpassword(e.target.value)} required />
+                    <input type='password' className='signup-input' placeholder='Confirm Password' value={cfmpwd} onChange={(e)=>setCfmpwd(e.target.value)} required />
                     <button type='submit' className='signup-button'>Create Account</button>
                     </form>
                 </div>
